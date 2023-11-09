fce4a2cffca19cda969e956f4b5a5f62fc42f260
nicolasroumec <nicolasroumec@hotmail.com>
2023-11-09 11:07:38 -0300

    Create model and service

---
+++ src/app/Models/student.model.ts
@@ -0,0 +1,12 @@
+export class Student{
+  id: number = 0;
+  dni!: number;
+  lastName!: string;
+  firstName!: string;
+  email!: string;
+  cohort: number = 0;
+  status: string = 'activo';
+  gender: string = 'masculino';
+  address: string = 'abc123';
+  phone: number = 0;
+}
--- src/app/Services/student.service.ts
+++ src/app/Services/student.service.ts
@@ -1,9 +1,31 @@
+import { Student } from './../Models/student.model';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
 
-  constructor() { }
+  url!:string;
+
+  constructor(private http: HttpClient) {
+    this.url = "https://b7b9-181-231-122-56.ngrok-free.app/student"
+  }
+
+  getStudents(){
+    return this.http.get(this.url + '/getAll')
+  }
+
+  addStudent(student:Student){
+    return this.http.post(this.url + "/", student)
+  }
+
+  updateStudent(id:number, student:Student){
+    return this.http.post(this.url + id + "/update", student)
+  }
+
+  deleteStudent(id:number, student:Student){
+    return this.http.post(this.url + id + "/delete", student)
+  }
 }
